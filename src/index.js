3bab6146d2052ebf7f948a615af556027ac51fbf
Cody Mikol <codymikol@gmail.com>
2019-08-15 03:26:02 -0400

    init(game) added a ton of boilerplate
    added webpack, procfile, bumped kiln, added
    express
    
    Fixes #5,#4,#2,#1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
-class TitleBounce extends Kiln.Entity {
+import { KEntity, KDraw, KGame, KScreen } from "../node_modules/game-kiln/dist/Kiln.bundle.js";
+
+class TitleBounce extends KEntity {
     constructor(x,y) {
         super(x,y);
         this.xvel = 8;
         this.yvel = 8;
     }
     onRender = () => {
-        let brush = new Kiln.Draw('game');
+        let brush = new KDraw('game');
         brush.text('Ok, the boilerplate is here, lets do it', this.x, this.y);
     };
     onTick = (dt) => {
+
         this.x += (dt / this.xvel);
         this.y += (dt / this.yvel);
+
+        if(this.x > 300 || this.x < 50) {
+            this.xvel *= -1;
+        }
+
+        if(this.y > 300 || this.y < 50) {
+            this.yvel *= -1;
+        }
+
     };
 }
 
-class TitleScreen extends Kiln.Screen {
+class TitleScreen extends KScreen {
     constructor() {super()}
-    onCreate() {this.add(new TitleBounce(1000, 500));}
+    onCreate() {this.add(new TitleBounce(100, 100));}
 }
 
-window.onload =  () => Kiln.Game('game', document.getElementById('game'), new TitleScreen());
+window.onload =  () => KGame('game', document.getElementById('game'), new TitleScreen());
 
---
+++ webpack.dev.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    target:'web',
+    mode: 'development',
+    devtool: 'source-map',
+    context: path.resolve(__dirname, './src'),
+    entry: {
+        Game: './index.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
