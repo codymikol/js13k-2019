97be209cb6c0193c024bc4ad88d746d4a239c023
flyguy <johnflynn493@gmail.com>
2019-08-31 14:27:15 -0400

    Added a door.. but it doesn't really look like a door.

--- src/Entities/Castle/Castle.js
+++ src/Entities/Castle/Castle.js
@@ -13,7 +13,9 @@ export default class Castle extends KEntity {
             this.width = 200;
             this.height = (this.brush.ctx.canvas.height * 0.75);
             this.x = (this.brush.ctx.canvas.width - this.width);
-            this.brush.square(this.x, this.y, this.width, this.height, '#2F343F')
+            this.brush.square(this.x, this.y, this.width, this.height, '#2F343F');
+            // Maybe make this an image? It doesn't look like a door at all...
+            this.brush.square(this.x + (this.width / 2.5), (this.brush.ctx.canvas.height * 0.50), this.width, (this.height / 3), '#CA6610');
         }
     }
 }
